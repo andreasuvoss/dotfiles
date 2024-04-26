6a60d1498ddff99aa2093f82b2a905c034907602
Andreas Voss <andreas@anvo.dk>
2024-04-26 11:36:24 +0200

    Show hidden files when using fuzzy finder

--- nvim/.config/nvim/lua/user/plugins/telescope.lua
+++ nvim/.config/nvim/lua/user/plugins/telescope.lua
@@ -5,8 +5,14 @@ return {
     lazy = false,
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      require("telescope").setup{
-path_display = { "smart" }
+        require("telescope").setup {
+            path_display = { "smart" },
+            pickers = {
+                find_files = {
+                    find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' },
+                    -- previewer = false
+                }
+            }
         }
     end
-  }
+}
